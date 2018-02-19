d49b7369072ce1d53ff775ddd69a9d241ddf7e70
birajgtm <birajgtm@gmail.com>
2018-02-20 00:22:29 +0530

    component state

--- public/app.jsx
+++ public/app.jsx
@@ -1,16 +1,52 @@
 var Greeter = React.createClass({
+
+    getDefaultProps: function() {
+        return{
+            name: "React",
+            message: "This is default message"
+        };
+    },
+
+    getInitialState: function() {
+        return {
+            name: this.props.name
+        };
+    },
+
+    onButtonClick: function(e){
+        
+        e.preventDefault();
+        
+        var nameRef = this.refs.name;
+        var name = nameRef.value;
+        nameRef.value = '';
+
+        if( typeof name === 'string' && name.length > 0){
+        this.setState({
+            name: name
+        });
+        }
+    },
+
     render: function(){
+        var name = this.state.name;
+        var message = this.props.message;
         return(
             <div>
-            <h1>HELLO REACt!</h1>
-            <p>This is from the component</p>
+            <h1>Hello {name}!</h1>
+            <p>{message + "!!"}</p>
+            <form onSubmit={this.onButtonClick}>
+                <input type="text" ref="name"/>
+                <button>Set Name</button>
+            </form>
             </div>
-            <div>2nd div</div>
         );
     }
 });
 
+var firstName = "Biraj";
+
 ReactDOM.render(
-    <Greeter/>,
+    <Greeter name={firstName}/>,
     document.getElementById("app")
 );
